1fc2cc9f0b8027b7d1eb9e40d68e03b8cddfcc84
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-11-20 16:26:03 -0300

    add endpoint mapping to sms service

--- src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -3,21 +3,23 @@ package com.devsuperior.dsmeta.controller;
 import com.devsuperior.dsmeta.domain.Sale;
 import com.devsuperior.dsmeta.service.SaleService;
 
+import com.devsuperior.dsmeta.service.SmsService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sales")
 public class SaleController {
 
     private final SaleService service;
+    private final SmsService smsService;
 
-    public SaleController(SaleService service) {
+    public SaleController(SaleService service, SmsService smsService) {
         this.service = service;
+        this.smsService = smsService;
     }
 
     @GetMapping
@@ -26,5 +28,11 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable){
         return service.findAll(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public ResponseEntity<String> sendSms(@PathVariable Long id) {
+        smsService.sendSms(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("SMS sent!");
     }
 }
