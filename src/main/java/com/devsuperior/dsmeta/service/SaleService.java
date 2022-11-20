8433ca11749209a4de4cccd24be9130f2e38c26b
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-11-20 15:43:00 -0300

    add sales service

--- src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -1,2 +1,21 @@
-package com.devsuperior.dsmeta.service;public class SaleService {
+package com.devsuperior.dsmeta.service;
+
+import com.devsuperior.dsmeta.domain.Sale;
+import com.devsuperior.dsmeta.repository.SaleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    private final SaleRepository saleRepository;
+
+    public SaleService(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
+    public List<Sale> findAll(){
+        return saleRepository.findAll();
+    }
 }
