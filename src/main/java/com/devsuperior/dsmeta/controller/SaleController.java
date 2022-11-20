11ad89e9f19b47eaa9bbab5ea7b1bc8c2eb8d17f
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-11-20 15:43:14 -0300

    add sales controller

--- src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -1,2 +1,25 @@
-package com.devsuperior.dsmeta.controller;public class SaleController {
+package com.devsuperior.dsmeta.controller;
+
+import com.devsuperior.dsmeta.domain.Sale;
+import com.devsuperior.dsmeta.service.SaleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sales")
+public class SaleController {
+
+    private final SaleService service;
+
+    public SaleController(SaleService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Sale> findAll(){
+        return service.findAll();
+    }
 }
